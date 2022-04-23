068200d38130e7fc3557e63f48aeaa38438bad32
PingbangHu <bensoniscool123@gmail.com>
2022-04-22 22:08:53 -0400

    📍[main/01] updates

--- 01_generate_instances.py
+++ 01_generate_instances.py
@@ -59,6 +59,15 @@ if __name__ == '__main__':
     filenames = []
     nums = []
 
+    # test instances
+    n = parameters.test_instance
+    num = int(tsp_size)
+    lp_dir = f'data/tsp{tsp_size}/instances/test_{num}n'
+    print(f"{n} instances in {lp_dir}")
+    os.makedirs(lp_dir)
+    filenames.extend([os.path.join(lp_dir, f'instance_{i+1}.lp') for i in range(n)])
+    nums.extend([num] * n)
+
     # train instances
     n = parameters.train_instance
     lp_dir = f'data/tsp{tsp_size}/instances/train_{num}n'
@@ -107,52 +116,41 @@ if __name__ == '__main__':
         print(f'  generating file {filename} ...')
         generate_tsp(n=num, filename=filename, random=random)
 
-    # test instances
-    nums = []
-    filenames = []
-    n = parameters.test_instance
-    num = int(tsp_size)
-    lp_dir = f'data/tsp{tsp_size}/instances/test_{num}n'
-    print(f"{n} instances in {lp_dir}")
-    os.makedirs(lp_dir)
-    filenames.extend([os.path.join(lp_dir, f'instance_{i+1}.lp') for i in range(n)])
-    nums.extend([num] * n)
+    # os.makedirs('results', exist_ok=True)
+    # concorde_result = f"concorde_{tsp_size}n_{time.strftime('%m%d-%H%M')}.csv"
+    # with open(f"results/{concorde_result}", 'w', newline='') as csvfile:
+    #     writer = csv.DictWriter(csvfile, fieldnames=['instance', ['walltime']])
+    #     # actually generate the instances
+    #     for filename, num in zip(filenames, nums):
+    #         print(f'  generating file {filename} ...')
+    #         cities = [i for i in range(num)]
+    #         edges = [(i, j) for i in cities for j in cities if i != j]
+    #         coord_x = random.rand(num) * 100
+    #         coord_y = random.rand(num) * 100
 
-    os.makedirs('results', exist_ok=True)
-    concorde_result = f"concorde_{tsp_size}n_{time.strftime('%m%d-%H%M')}.csv"
-    with open(f"results/{concorde_result}", 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=['instance', ['walltime']])
-        # actually generate the instances
-        for filename, num in zip(filenames, nums):
-            print(f'  generating file {filename} ...')
-            cities = [i for i in range(num)]
-            edges = [(i, j) for i in cities for j in cities if i != j]
-            coord_x = random.rand(num) * 100
-            coord_y = random.rand(num) * 100
+    #         start_time = time.time()
+    #         solver = TSPSolver.from_data(coord_x, coord_y, norm="GEO")
+    #         solution = solver.solve()
+    #         end_time = time.time() - start_time
+    #         writer.writerow({
+    #             'instance': filename,
+    #             'walltime': end_time,
+    #         })
+    #         csvfile.flush()
 
-            start_time = time.time()
-            solver = TSPSolver.from_data(coord_x, coord_y, norm="GEO")
-            solution = solver.solve()
-            end_time = time.time() - start_time
-            writer.writerow({
-                'instance': filename,
-                'walltime': end_time,
-            })
-            csvfile.flush()
+    #         distances = {(i, j): np.hypot(coord_x[i] - coord_x[j], coord_y[i] - coord_y[j]) for i, j in edges}
 
-            distances = {(i, j): np.hypot(coord_x[i] - coord_x[j], coord_y[i] - coord_y[j]) for i, j in edges}
-
-            mdl = Model('TSP')
-            x = mdl.binary_var_dict(edges, name='x')
-            d = mdl.continuous_var_dict(cities, name='d')
-            mdl.minimize(mdl.sum(distances[i] * x[i] for i in edges))
-            for c in cities:
-                mdl.add_constraint(mdl.sum(x[(i, j)] for i, j in edges if i == c) == 1, ctname='out_%d' % c)
-                mdl.add_constraint(mdl.sum(x[(i, j)] for i, j in edges if j == c) == 1, ctname='in_%d' % c)
-            for i, j in edges:
-                if j != 0:
-                    mdl.add_indicator(x[(i, j)], d[i] + 1 == d[j], name='order_(%d,_%d)' % (i, j))
-            print(filename)
-            mdl.export_as_lp(filename)
+    #         mdl = Model('TSP')
+    #         x = mdl.binary_var_dict(edges, name='x')
+    #         d = mdl.continuous_var_dict(cities, name='d')
+    #         mdl.minimize(mdl.sum(distances[i] * x[i] for i in edges))
+    #         for c in cities:
+    #             mdl.add_constraint(mdl.sum(x[(i, j)] for i, j in edges if i == c) == 1, ctname='out_%d' % c)
+    #             mdl.add_constraint(mdl.sum(x[(i, j)] for i, j in edges if j == c) == 1, ctname='in_%d' % c)
+    #         for i, j in edges:
+    #             if j != 0:
+    #                 mdl.add_indicator(x[(i, j)], d[i] + 1 == d[j], name='order_(%d,_%d)' % (i, j))
+    #         print(filename)
+    #         mdl.export_as_lp(filename)
 
     print('done.')
